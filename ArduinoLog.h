08a8324f0b8910d7250c9fc6f6605d154a45ec18
Felix Mitterer <felix.mitterer@technikum-wien.at>
2018-06-11 09:51:29 +0200

    Fixed Error when writing Strings from Flash memory
    When writing messages from Flash to SD or Serial, errors can have occurred. Reason for that beeing the used vsnprintf function that formats the string. In particular, the method uses the pointer to the formated string as a reference for the variadic argument list. Bevor this commit, the String to print was first read from flash into memory, and this was handed to the vsnprintf function which then did not no the end of the variadic list. From thhis commit on, the vsnprintf_P function is used instead, which takes the pointer to the Flash memory position instead and can thus handle the variable argument list.

---
+++ ArduinoLog.h
@@ -0,0 +1,86 @@
+/*
+ * ArduinoLog.h
+ *
+ *  Created on: 03.06.2018
+ *      Author: felix
+ */
+
+#ifndef MPU9250_ARDUINO_LIBRARY_MPU9250_ARDUINO_DRIVER_ARDUINOLOG_H_
+#define MPU9250_ARDUINO_LIBRARY_MPU9250_ARDUINO_DRIVER_ARDUINOLOG_H_
+
+#include <WString.h>
+#include <SPI.h>
+#include <SD.h>
+
+#define AL_MAXBUFFER 32
+#define AL_MAXFILELENGTH 12
+#define AL_CARD_DETECT 9
+#define AL_CHIP_SELECT 10 //TODO:By using PIN 10 (PB2) as Chip Select. The SPI Cannot be used in Slave Mode. Change this for using Slafe mode
+
+#define LOG_FILE_NAME "ALog.txt" //Note, the Maximum filename length that the SD library can handle is 8.3 (eg: demofile.txt)
+
+class ArduinoLog {
+
+	//private Member Variables:
+	//As an alternative, the following could also be dynamically allocated using malloc and free
+//	char buff[AL_MAXBUFFER];	//Buffer to format the Data
+//	char dynbuff[AL_MAXBUFFER];	//Buffer to dynamically load data from FLASH
+	char* buff=NULL;	//Buffer to format the Data
+	char* buff_=NULL;	//Buffer to format the Data
+	char* formatbuff=NULL;	//Buffer to dynamically load data from FLASH
+	char* formatbuff_=NULL;	//Buffer to dynamically load data from FLASH
+	//char* resizedBuff = NULL;
+	bool sdAlreadyInitialized=false;
+	char logFile[AL_MAXFILELENGTH];
+	File file;
+
+
+public:
+	//Methods:
+	ArduinoLog();
+	virtual ~ArduinoLog();
+	/*
+	 * This Method prints Log Messages to the Serial Console
+	 * Use this Method as you would use printf()
+	 */
+	void logSerial(const char* format,...);
+
+	/*
+	 * This Method prints Log Messages to the Serial Console
+	 * Use this Method as you would use printf()
+	 * In difference to logSerial, the Message is sotred in Flash, and not in RAM.
+	 * It needs to be called: logSerial(F("Some %d formated %f string: %s"),x,y,z)
+	 */
+	void logSerial(const __FlashStringHelper* format,...);
+
+	/*
+	 * This Method prints Log Messages to the SD card
+	 * Use this Method as you would use printf()
+	 * It needs to be called: logSD("Some %d formated %f string: %s",x,y,z)
+	 */
+	void logSD(const char* format,...);
+	/*
+	 * This Method prints Log Messages to the SD card
+	 * Use this Method as you would use printf()
+	 * In difference to logSD, the Message is sotred in Flash, and not in RAM.
+	 * It needs to be called: logSD(F("Some %d formated %f string: %s"),x,y,z)
+	 */
+	void logSD(const __FlashStringHelper* format,...);
+
+	/*
+	 * Set the name of the File to be logged to
+	 * NOTE: Max file name length that is supported by the SD library is:
+	 * 8.3 (eg: demofile.txt will work, longfilename.txt wont)
+	 * returns true on success
+	 */
+	bool setLogFileName(char* name);
+
+	void logError();
+
+private:
+	bool initializeSD();
+};
+
+extern ArduinoLog ALog;
+
+#endif /* MPU9250_ARDUINO_LIBRARY_MPU9250_ARDUINO_DRIVER_ARDUINOLOG_H_ */
